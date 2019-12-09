cab2f01e4a90ea6d8e1017b12bae53bdd38857bc
Farid <thesomeq@gmail.com>
2019-12-09 20:00:14 +0600

    Добавлена заготовка для приложения API и подключены drf b drf.authtoken.

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'webapp',
+    'api_v1'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
