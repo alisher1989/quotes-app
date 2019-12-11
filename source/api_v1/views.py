1172d368e4838dad53a2b6d1a3a638bc4ac5c10b
Farid <thesomeq@gmail.com>
2019-12-11 22:40:10 +0600

    Пример представления для повышения рейтинга цитаты на 1 на базе APIView.

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -7,7 +9,7 @@ from webapp.models import Quote, QUOTE_APPROVED
 
 
 class LogoutView(APIView):
-    permission_classes = []
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         user = self.request.user
@@ -28,3 +30,15 @@ class QuoteViewSet(ModelViewSet):
     # доступ к изменению и удалению цитат
     # только для вошедших пользователей
     # ...
+
+
+class RateUpView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, pk=None):
+        quote = get_object_or_404(Quote, pk=pk)
+        if quote.status != QUOTE_APPROVED:
+            return Response({'error': 'Цитата не утверждена'}, status=403)
+        quote.rating += 1
+        quote.save()
+        return Response({'id': quote.pk, 'rating': quote.rating})
